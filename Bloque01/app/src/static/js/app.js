bb5c133064e52dc3378a5bb23c56da2c8fee639c
interactivedevelop <sergi@systtekinteractive.com>
2022-04-16 17:14:03 +0200

    travis file repair

--- Bloque01/app/src/static/js/app.js
+++ Bloque01/app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">¡Aún no hay Items!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New Item"
+                    placeholder="Nuevo Item"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Items'}
+                        {submitting ? 'Adding...' : 'Añadir Item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
